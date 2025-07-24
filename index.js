b13d0a29a6e68cabd7bbacd777980d8f955dc072
SubhamayDebnath <subhamaydebnath6@gmail.com>
2025-07-24 13:58:07 +0530

    add books and fetch user data

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get("/", async (req, res) => {
 // add subscriber 
 app.post("/api/add-subscriber",async(req,res)=>{
     try {
-        const {email,first_name,last_name} = req.body;
+        const {email,first_name,last_name,books} = req.body;
         if(!email || !isValidEmail(email)){
             return res.status(400).json({error:"Invalid email"});
         }
@@ -44,11 +44,31 @@ app.post("/api/add-subscriber",async(req,res)=>{
             status: "subscribed",
             merge_fields: {
                 FNAME: firstName,
-                LNAME: lastName
+                LNAME: lastName,
+                BOOKS: books
             }
         });
-        res.json({message:"Subscriber Added Successfully",response:response});
+        res.status(200).json({message:"Subscriber Added Successfully",response:response});
         
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:error.message})
+    }
+})
+
+app.get("/api/get-subscribers",async(req,res)=>{
+    try {
+        const response = await mailchimp.lists.getListMembersInfo(process.env.MAILCHIMP_LIST_ID);
+        response.members.forEach((member) => {
+            console.log({
+              email: member.email_address,
+              status: member.status,
+              firstName: member.merge_fields.FNAME,
+              lastName: member.merge_fields.LNAME,
+              books: member.merge_fields.BOOKS, // Your custom field
+            });
+        });
+        res.status(200).json({message:"Subscriber Fetched Successfully",response:response});
     } catch (error) {
         console.log(error);
         res.status(500).json({error:error.message})
